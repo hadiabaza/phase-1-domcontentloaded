7f016ed803606f7b57930d7c8da44bee69cb941f
Hadi Abaza <Hadiabaza1@gmail.com>
2021-06-12 15:05:31 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+document.addEventListener(
+  "DOMContentLoaded",
+  () => (document.querySelector("#text").textContent = "This is really cool!")
+);
+
+const firstDiv = document.createElement("div");
+firstDiv.id = "container";
+
+let h1 = document.createElement("h1");
+
+firstDiv.appendChild(h1);
+
+h1.textContent = "Pizza";
+
+document.body.insertBefore(firstDiv, document.querySelector("p"));
+
+let image = document.createElement("img");
+image.src = "https://placekitten.com/400/900";
+
+firstDiv.appendChild(image);
